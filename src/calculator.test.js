9fa5b69240a13bf8ceab1761cd1a5a01a5fbe32b
Albert <stamina.left@gmail.com>
2021-06-07 12:34:15 +0700

    Created rendering spots.

--- src/calculator.test.js
+++ src/calculator.test.js
@@ -3,3 +3,11 @@ import { render } from 'react-dom';
 import { act } from 'react-dom/test-utils';
 
 import Calculator from './calculator';
+
+let container = null;
+beforeEach(() => {
+    // Atur elemen DOM sebagai tujuan render
+    container = document.createElement("div");
+    // container *harus* melekat pada document agar events bekerja dengan benar
+    document.body.appendChild(container);
+});
