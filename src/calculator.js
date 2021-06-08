21ab836e0e4ed244663d54a2fd0a0ed4d074a7b9
Albert <stamina.left@gmail.com>
2021-06-08 15:51:21 +0700

    Separates a Calculator and an EntryPad.

--- src/calculator.js
+++ src/calculator.js
@@ -1,7 +1,19 @@
 import React from 'react';
 
 export default function Calculator(props) {
+    const keys = props.keys;
+    const entryPads = keys.map((key) => 
+        <EntryPad key={key} entryPadKey={key} />
+    );
     return (
-        <button>{ props.entryPad }</button>
+        <div id="entry-pads">{ entryPads }</div>
+    );
+}
+
+function EntryPad(props) {
+    return (
+        <button name={ props.entryPadKey }>
+            { props.entryPadKey }
+        </button>
     );
 }
