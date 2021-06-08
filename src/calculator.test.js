1ab5daa882666c94467472cade3d27e1b4539e42
Albert <stamina.left@gmail.com>
2021-06-08 15:50:50 +0700

    Refactored using 'it.each' to check each entryPad.

--- src/calculator.test.js
+++ src/calculator.test.js
@@ -1,29 +1,23 @@
 import React from 'react';
-import { render } from 'react-dom';
-import { act } from 'react-dom/test-utils';
+import { render, screen } from '@testing-library/react';
+import '@testing-library/jest-dom';
 
 import Calculator from './calculator';
 
-let container = null;
-beforeEach(() => {
-    // Atur elemen DOM sebagai tujuan render
-    container = document.createElement("div");
-    // container *harus* melekat pada document agar events bekerja dengan benar
-    document.body.appendChild(container);
-});
+describe("Renders a calculator with entry pads", () => {
 
-it("renders an entry pads", () => {
-    const numberEntryPads = Array.from(Array(10).keys());
-    const operationEntryPads = ["+", "-", "/", "="];
-    const helperEntryPads = ["C", "AC"];
-    
-    const entryPads = numberEntryPads
-        .concat(operationEntryPads, helperEntryPads);
-    
-    entryPads.forEach(entryPad => {
-        act(() => {
-            render(<Calculator entryPad={entryPad} />, container);
-        });
-        expect(container.textContent).toBe(`${entryPad}`);
+    const numberKeys = Array.from(Array(10).keys());
+    const operationKeys = ["+", "-", "/", "="];
+    const helperKeys = ["C", "AC"];
+
+    const entryPadsKey = numberKeys
+        .concat(operationKeys, helperKeys);
+
+    it.each(entryPadsKey)(`Check if it renders a %s entry pad.`, 
+    (entryPad) => {
+        render(<Calculator keys={entryPadsKey} />);
+        const entryPadElement = screen.getByRole('button', 
+        {name: entryPad});
+        expect(entryPadElement).toBeInTheDocument();
     });
 });
